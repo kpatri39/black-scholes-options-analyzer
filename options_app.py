61cba5873f386b7a7c84b8acf137028a541f3959
Kartik Patri <kartikpatri39@gmail.com>
2025-09-23 22:16:50 -0700

    Fix Vercel deployment dependencies

--- options_app.py
+++ options_app.py
@@ -72,7 +72,6 @@ def theory():
     return render_template('theory.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-# For Vercel
-app = app
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
